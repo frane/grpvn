85921146be158ec86c330d00d22c8266690a9862
Frane Bandov <frane@offbyte.com>
2026-07-15 14:46:02 +0200

    Bump version to 0.3.0

--- cmd/grpvn/root.go
+++ cmd/grpvn/root.go
@@ -40,7 +40,7 @@ var (
 	countFlag                        int
 
 	// Overridden by goreleaser via -X main.version / main.commit / main.date.
-	version = "0.2.0"
+	version = "0.3.0"
 	commit  = ""
 	date    = ""
 )
